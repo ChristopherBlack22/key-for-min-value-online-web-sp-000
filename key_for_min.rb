fb0287c8ac6512ea7fd1da6957fe2c49bdb31d58
Christopher Black <christopher.black.2209@googlemail.com>
2021-04-08 16:11:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,5 +5,10 @@ require 'pry'
 
 def key_for_min_value(name_hash)
   value_array = name_hash.collect {|key, value| value}.sort {|a,b| a<=>b} 
-  value_array
+  name_hash.each do |key,value| 
+    if value == value_array[0]
+      return key
+    end
+  end
+  nil 
 end
