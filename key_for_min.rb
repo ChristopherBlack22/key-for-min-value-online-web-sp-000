54e6263cb21d9dfa347f734323e2e6f36b13788e
Christopher Black <christopher.black.2209@googlemail.com>
2021-04-08 16:06:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,6 @@
 require 'pry'
 
 def key_for_min_value(name_hash)
-  name_hash.collect {|key, value| value}.sort {|a,b| a<=>b} 
+  value_array = name_hash.collect {|key, value| value}.sort {|a,b| a<=>b} 
   binding.pry
 end
